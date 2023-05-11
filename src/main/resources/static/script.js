0224e06f212802be404af542e3ebae9e1f27554e
Orozbai Altynbekov <altynbekov.bai@gmail.com>
2023-05-12 01:04:13 +0600

    added function clear orders,

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -6,3 +6,22 @@ async function getProducts(e) {
         console.log(data);
     }));
 }
+
+document.getElementById('search-form').addEventListener('submit', function (e) {
+    e.preventDefault();
+    let formData = new FormData(e.target);
+    const value = formData.get('s');
+    console.log(value);
+});
+
+document.getElementById('orders-button-clear').addEventListener('click', function (e) {
+    e.preventDefault();
+    let list = document.getElementById('orders-ol');
+    while (list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
+    let emptyListItem = document.createElement('li');
+    emptyListItem.id = 'clear-li-text';
+    emptyListItem.textContent = 'Корзина пуста';
+    list.appendChild(emptyListItem);
+});
