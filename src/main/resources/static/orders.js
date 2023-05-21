96ca58cf112e31f51f6404c7aee46ee4adef132b
Orozbai Altynbekov <altynbekov.bai@gmail.com>
2023-05-22 05:16:16 +0600

    added modal window

--- src/main/resources/static/orders.js
+++ src/main/resources/static/orders.js
@@ -69,6 +69,7 @@ function insertCart(form) {
     count.setAttribute('value', '1');
     count.setAttribute('min', '1');
     count.setAttribute('max', form.get('count'));
+    count.setAttribute('oninput', 'checkValue(this,' + form.get('count') + ' )');
     div.appendChild(label);
     div.appendChild(count);
 
@@ -100,7 +101,6 @@ function deleteBasket(id, event) {
     data.forEach((obj, index) => {
         if (obj.id === num) {
             data.splice(index, 1);
-            console.log("splice")
         }
     });
     const newJsonList = JSON.stringify(data);
@@ -110,9 +110,81 @@ function deleteBasket(id, event) {
 
 const cartDetails = document.getElementById('cart-details');
 if (cartDetails) {
-    document.getElementById('cart-details').addEventListener('click', function (e) {
-       e.preventDefault();
+    cartDetails.addEventListener('click', showModal);
+}
 
+function showModal() {
+    const deleteElements = document.getElementById('modal-content-text');
+    while (deleteElements.firstChild) {
+        deleteElements.removeChild(deleteElements.firstChild);
+    }
+    const pTotal = document.createElement('p');
+    pTotal.textContent = 'Итоговая сумма';
+    deleteElements.appendChild(pTotal);
 
-    });
+    const pPrice = document.createElement('p');
+    pPrice.id = 'total-price';
+    pPrice.innerHTML = '0';
+    deleteElements.appendChild(pPrice);
+
+    const modal = document.getElementById('myModal');
+    const closeBtn = document.getElementsByClassName("close")[0];
+
+    if (modal.style.display === 'none') {
+        modal.style.display = 'block';
+    }
+    closeBtn.onclick = function () {
+        modal.style.display = 'none';
+    }
+
+    window.onclick = function (event) {
+        if (event.target === modal) {
+            modal.style.display = 'none';
+        }
+    }
+
+    const jsonArray = sessionStorage.getItem('products');
+    if (jsonArray) {
+        const objectArray = JSON.parse(jsonArray);
+
+        for (const p of objectArray) {
+            const form = new FormData();
+            form.append('name', p.name);
+            form.append('count', p.count);
+            form.append('price', p.price);
+            form.append('id', 'count-' + p.id);
+            insertModal(form);
+        }
+    }
+}
+
+function insertModal(form) {
+    const div = document.createElement('div');
+    const p = document.createElement('p');
+    p.textContent = 'name: ' + form.get('name');
+    div.appendChild(p);
+
+    const price = document.createElement('p');
+    price.textContent = 'price: ' + form.get('price');
+    div.appendChild(price);
+
+    const inputCount = document.getElementById(form.get('id')).value;
+    const count = document.createElement('p');
+    count.textContent = 'count: ' + inputCount;
+    div.appendChild(count);
+
+    const totalPrice = document.getElementById('total-price');
+    const number = parseInt(totalPrice.textContent);
+    const intPrice = parseInt(form.get('price'));
+    const intCount = parseInt(inputCount);
+    totalPrice.innerHTML = number + (intPrice * intCount);
+
+    const divElem = document.getElementById('modal-content-text');
+    divElem.appendChild(div);
+}
+
+function checkValue(input, num) {
+    if (input.value > num && input.value > 0) {
+        input.value = '';
+    }
 }
