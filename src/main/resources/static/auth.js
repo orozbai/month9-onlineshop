5adf08c3b6b4607d7c62b6d9bab175e370efdfec
Orozbai Altynbekov <altynbekov.bai@gmail.com>
2023-05-22 17:56:10 +0600

    register captcha added

--- src/main/resources/static/auth.js
+++ src/main/resources/static/auth.js
@@ -6,18 +6,41 @@ if (registerForm != null) {
         event.preventDefault();
         let formData = new FormData(event.target);
         let loginFormData = new FormData();
-        loginFormData.append('password', document.getElementById('register-password').value);
-        const email = document.getElementById('register-email').value;
-        loginFormData.append('email', email);
-        const csrfToken = document.querySelector('meta[name="_csrf_token"]').getAttribute('content');
-        await fetch('http://localhost:8089/register', {
-            method: 'POST',
-            headers: {
-                'Content-type': 'application/json',
-                'X-CSRF-TOKEN': csrfToken
-            },
-            body: JSON.stringify(Object.fromEntries(formData))
-        });
-        window.location.href = 'http://localhost:8089/login';
+        if (checkRegisterCaptcha() !== true) {
+            alert("Капча введена не правильно");
+            window.location.href = 'http://localhost:8089/register';
+        } else {
+            loginFormData.append('password', document.getElementById('register-password').value);
+            const email = document.getElementById('register-email').value;
+            loginFormData.append('email', email);
+            const csrfToken = document.querySelector('meta[name="_csrf_token"]').getAttribute('content');
+            await fetch('http://localhost:8089/register', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json',
+                    'X-CSRF-TOKEN': csrfToken
+                },
+                body: JSON.stringify(Object.fromEntries(formData))
+            });
+            window.location.href = 'http://localhost:8089/login';
+        }
     })
+}
+
+const firstNum = generateRandomNumber();
+const secondNum = generateRandomNumber();
+const captcha = document.getElementById('captcha-register');
+
+if (window.location.href === 'http://localhost:8089/register') {
+    captcha.setAttribute('placeholder', '' + firstNum + "+" + secondNum + '');
+}
+
+function checkRegisterCaptcha() {
+    const answer = firstNum + secondNum;
+    const number = parseInt(captcha.value);
+    return answer === number;
+}
+
+function generateRandomNumber() {
+    return Math.floor(Math.random() * 10);
 }
